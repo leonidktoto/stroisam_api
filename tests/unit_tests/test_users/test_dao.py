616c198b72a80afdd201be8638083f3bbbe679d5
Leonid <leonidktoto89@icloud.com>
2024-12-15 14:09:56 +0400

    test: Добавить тест на поиск смс кодов по id пользователя
    -добавлен тест test_last_sms_code в с двумя параметрами

--- tests/unit_tests/test_users/test_dao.py
+++ tests/unit_tests/test_users/test_dao.py
@@ -1,5 +1,6 @@
 from app.users.dao import UsersDAO
-
+from app.users.sms_codes.dao import SmsCodesDAO
+import pytest
 
 async def test_add_and_get_users():
     new_user = await UsersDAO.add_data(
@@ -9,9 +10,21 @@ async def test_add_and_get_users():
         email="denis@mail.ru"
     )
 
-
     new_user = await UsersDAO.find_one_or_none(id=new_user["id"])
-    print(new_user)
+    
     assert new_user is not None
 
 
+
+
+@pytest.mark.parametrize("user_id, expected_sms_code_exists", [
+    (1,True ),
+    (9, False)
+])
+
+async def test_last_sms_code(user_id,expected_sms_code_exists):
+    sms_code = await SmsCodesDAO.last_sms_code(user_id=user_id)
+    if expected_sms_code_exists:
+        assert sms_code is not None
+    else:
+        assert sms_code is None
