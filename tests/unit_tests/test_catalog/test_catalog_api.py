b084a71ccc2d913cc2d10d01fabed05aef6223ed
Leonid <leonidktoto89@icloud.com>
2024-12-17 20:16:56 +0400

    test: Добавить api test по фильтру товара
    -Добавлен тест test_get_product_by_filter

--- tests/unit_tests/test_catalog/test_catalog_api.py
+++ tests/unit_tests/test_catalog/test_catalog_api.py
@@ -108,9 +108,85 @@ async def test_get_products_by_category_id(category_id, status_code,expected_res
 @pytest.mark.asyncio
 async def test_get_products_by_id(id, status_code,expected_response, ac: AsyncClient):
     response = await ac.get(f"/products/id/{id}", follow_redirects=True)
-    print("!!!!")
-    print(response.content)
     assert response.status_code == status_code
     
     if expected_response is not None:
         assert response.json() == expected_response
+
+
+
+
+@pytest.mark.parametrize(
+    "category_id, filters, status_code, expected_response",
+        [
+            (24, [
+            {
+                "attribute_name": "Материал",
+                "attribute_value": [
+                "Сосна", "Береза", "Хвоя"
+                ]
+            },
+            {
+                "attribute_name": "Сорт",
+                "attribute_value": [
+                "Люкс", "Оптима"
+                ]
+            },
+            {
+                "attribute_name": "Размер",
+                "attribute_value": [
+                "40x50x2000 мм", "20x40x2000 мм"
+                ]
+            }
+            ], 200, [
+            {
+                "id": 14,
+                "article": 212378906,
+                "product_name": "Рейка строганая 40x50x2000 мм хвоя сорт Оптима",
+                "price": 120,
+                "image_url": None
+            }]
+            ),
+            (
+                24, [{"test":"test"}], 422, None   
+            ),
+            (
+                "24", [{"test":"test"}], 422, None   
+            ),
+            (
+                24, None , 422, None   
+            ),
+            (24, [
+            {
+                "attribute_name": "Размер",
+                "attribute_value": [
+                "40x50x2000 мм", "20x40x2000 мм"
+                ]
+            }
+            ], 200, [
+            {
+                "id": 14,
+                "article": 212378906,
+                "product_name": "Рейка строганая 40x50x2000 мм хвоя сорт Оптима",
+                "price": 120,
+                "image_url": None
+            }]
+            ),
+
+        ]
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_product_by_filter(category_id, filters, status_code, expected_response, ac: AsyncClient):
+    payload = {"filters": filters, "category_id": category_id}
+    print(filters)
+
+    response = await ac.post(f"/products/filter?category_id={category_id}",  json=filters, follow_redirects=True)
+    assert response.status_code == status_code
+    
+    if expected_response is not None:
+        assert response.json() == expected_response
+
+
+   
