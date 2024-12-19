5be00eb408e054fd6867c8778fe12bb7f1c00d8b
Leonid <leonidktoto89@icloud.com>
2024-12-19 22:53:25 +0400

    test: Добавить тесты работы с корзиной
        - Добавлен тест работы с корзиной для не аутентифицированного пользвотеля test_carts_no_auth_user
        - Добавлен тест добавления в корзину test_add_item_to_cart
        - Добавлен тест получение из корзины сведений о товаре test_get_cart_contents
        - Добавлен тест удаления одного товара из корзины test_remove_item_from_cart
        - Добавлен тест очистки корзины test_clear_cart

--- tests/unit_tests/test_carts/test_carts_api.py
+++ tests/unit_tests/test_carts/test_carts_api.py
@@ -1,22 +1,7 @@
 from httpx import AsyncClient
 import pytest
 
-#@pytest.mark.asyncio
-#async def test_carts_no_auth_user(ac: AsyncClient):
-#    response = await ac.get("/users/cart/", follow_redirects=True)
-#    assert response.status_code == 401
-#    response = await ac.post("/users/cart/items", follow_redirects=True)
-#    assert response.status_code == 401
-#    response = await ac.delete("/users/cart/items", follow_redirects=True)
-#    assert response.status_code == 401
-#    response = await ac.patch("/users/cart/items", follow_redirects=True)
-#    assert response.status_code == 401
-#    response = await ac.delete("/users/cart/items/1", follow_redirects=True)
-#    assert response.status_code == 401
-
-
-@pytest.mark.parametrize(
-    "product, status_code",[
+test_data = [
         (
             {
                 "product_id": 0,
@@ -52,15 +37,57 @@ import pytest
             },
             422
         )
-    ])
+    ]
+
+@pytest.mark.asyncio
+async def test_carts_no_auth_user(ac: AsyncClient):
+    response = await ac.get("/users/cart/", follow_redirects=True)
+    assert response.status_code == 401
+    response = await ac.post("/users/cart/items", follow_redirects=True)
+    assert response.status_code == 401
+    response = await ac.delete("/users/cart/items", follow_redirects=True)
+    assert response.status_code == 401
+    response = await ac.patch("/users/cart/items", follow_redirects=True)
+    assert response.status_code == 401
+    response = await ac.delete("/users/cart/items/1", follow_redirects=True)
+    assert response.status_code == 401
+
+
+
+@pytest.mark.parametrize(
+    "product, status_code", test_data)
 @pytest.mark.asyncio
 async def test_add_item_to_cart(product, status_code, authenticated_ac: AsyncClient):
     response = await authenticated_ac.post("/users/cart/items", json=product, follow_redirects=True)
     assert response.status_code == status_code
 
+@pytest.mark.parametrize(
+    "product, status_code", test_data)
+@pytest.mark.asyncio
+async def test_partial_update_item(product, status_code, authenticated_ac: AsyncClient):
+    response = await authenticated_ac.patch("/users/cart/items", json=product, follow_redirects=True)
+    assert response.status_code == status_code
 
 @pytest.mark.asyncio
 async def test_get_cart_contents(authenticated_ac: AsyncClient):
-    response = await authenticated_ac.get("/users/cart/", follow_redirects=True)
+    response = await authenticated_ac.get("/users/cart", follow_redirects=True)
     assert response.json()[0]["id"] == 1
     assert response.status_code == 200
+
+@pytest.mark.parametrize("id, status_code",
+    [
+        (1,204),
+        (0,204),
+        ("1",204),
+        ("sdas",422)
+    ]  
+)
+@pytest.mark.asyncio
+async def test_remove_item_from_cart(id, status_code, authenticated_ac: AsyncClient):
+    response = await authenticated_ac.delete(f"/users/cart/items/{id}", follow_redirects=True)
+    assert response.status_code == status_code
+
+@pytest.mark.asyncio
+async def test_clear_cart(authenticated_ac: AsyncClient):
+    response = await authenticated_ac.delete("/users/cart/items", follow_redirects=True)
+    assert response.status_code == 204
