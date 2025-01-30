e8455d88a1edfafec9cfb057fbe55fe4eda2372a
Leonid <leonidktoto89@icloud.com>
2025-01-30 22:27:32 +0400

    ci: Добавлена отладочная информация

--- docker/app.sh
+++ docker/app.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo "Старт docker/app.sh ..."
 alembic upgrade head    
 
 if [ "$1" = "gunicorn" ]; then
+    echo "Старт gunicorn..."
     gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 elif [ "$1" = "pytest" ]; then
+    echo "Старт pytest..."
     pytest -c ci_pytest.ini -vv
 fi
